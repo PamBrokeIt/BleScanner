bb9337df88fc3421c00e42f1518d8f5d4450cd4f
Manuel <mouzomarsella@gmail.com>
2019-10-16 09:50:57 +0200

    Creamos los objetos BT

--- app/src/main/java/com/mamoumar/example/blescanner/MainActivity.java
+++ app/src/main/java/com/mamoumar/example/blescanner/MainActivity.java
@@ -2,16 +2,34 @@ package com.mamoumar.example.blescanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.content.Context;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    //------------------------------------------
+    //Variables
+    //------------------------------------------
+    BluetoothManager miBlueManager;
+    BluetoothAdapter miBlueAdapter;
+    BluetoothLeScanner miBlueScanner;
+    //------------------------------------------
+    //Variables
+    //------------------------------------------
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Creamos los objetos blManager blAdapter y blScanner
+        miBlueManager = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
+        miBlueAdapter = miBlueManager.getAdapter();
+        miBlueScanner = miBlueAdapter.getBluetoothLeScanner();
 
     }
 }
